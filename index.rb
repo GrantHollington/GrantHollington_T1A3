0e5fe3d9271f4bde36223e3ddbd3819e1208fc6f
GrantHollington <grant.j.hollington@gmail.com>
2021-05-18 11:32:34 +1000

    initial commit with user story

---
+++ index.rb
@@ -0,0 +1,6 @@
+# user story:
+# user must be able to view list of clothing
+# user must be able to choose clothing
+# user must be able to specify quantity of that item
+# user  should be able to order more than one item
+# user should be given the final bill
