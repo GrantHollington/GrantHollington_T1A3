2b3ec3e920c5db5e955d5da48d680b1c272b4f70
GrantHollington <grant.j.hollington@gmail.com>
2021-05-18 13:54:58 +1000

    add gems and gemfile

---
+++ spec/customer_order_spec.rb
@@ -0,0 +1,15 @@
+# test cases:
+# 1. test if the customer instance is created and the name is readable
+# 2. test should display a welcome message
+# 3. test should read the list of clothing 
+# 4. should be able to get user input/choice
+# 5. should be able to get the item price
+# 6. should be able to get user quantity
+# 7. should be able to get the final bill
+
+require 'colorize'
+puts "This is blue".colorize(:blue)
+
+require 'chuck_norris_jokes'
+chuck_norris = ChuckNorrisJokes::ChuckNorris.new
+puts chuck_norris.tell_me_a_joke_now
