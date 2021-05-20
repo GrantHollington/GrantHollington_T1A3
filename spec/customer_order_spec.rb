d0715233b156d6ad9b00f38c1e7acff3ac4758df
GrantHollington <grant.j.hollington@gmail.com>
2021-05-20 22:42:22 +1000

    add initial test code

---
+++ spec/customer_order_spec.rb
@@ -0,0 +1,25 @@
+# test cases:
+# 1. test if the customer instance is created and the name is readable (create customer instance)
+# 2. test should display a welcome message
+# 3. test should read the list of clothing 
+# 4. should be able to get user input/choice
+# 5. should be able to get the item price
+# 6. should be able to get user quantity
+# 7. should be able to get the final bill
+
+# require 'colorize'
+# puts "This is blue".colorize(:blue)
+
+# require 'chuck_norris_jokes'
+# chuck_norris = ChuckNorrisJokes::ChuckNorris.new
+# puts chuck_norris.tell_me_a_joke_now
+
+describe CustomerOrder do
+    before (:each) do
+        #running this code before each test case defined 
+        @customer = CustomerOrder.new("Bob")
+    end
+    it "must have a readable name" do
+        expect(@customer.name).to eq("Bob")
+end
+
